73090def0677f3143e5097d0b884d033f04106a9
Ayush pal <aarushpal876@gmail.com>
2026-06-18 13:59:30 +0530

    Update models.py

--- ecommerceapp/models.py
+++ ecommerceapp/models.py
@@ -15,7 +15,7 @@ class AddProduct(models.Model):
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=255)
     available = models.BooleanField(default=True)
-    pic = models.ImageField(upload_to='media/products/', blank=True, null=True)
+    pic = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
         return self.name
