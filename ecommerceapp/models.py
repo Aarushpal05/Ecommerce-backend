75b3b070cd2605660effab7c29990cbe136294ac
Ayush pal <aarushpal876@gmail.com>
2026-06-23 14:40:06 +0530

    Update image upload path for category model

--- ecommerceapp/models.py
+++ ecommerceapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class category(models.Model):
     name= models.CharField(max_length=255)
-    pic= models.ImageField(upload_to='media/category/', blank=True, null=True )
+    pic= models.ImageField(upload_to='category/', blank=True, null=True )
 
     def __str__(self):
         return self.name
